3c0e2dfc0cb9aa2418f30a844f09e0f652d6c297
Bruno Costa <euobrunocosta@gmail.com>
2020-03-14 16:29:44 -0300

    melhor a formatação do arquivo de rotas para apis

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 // listar cidadãos
 Route::get('cidadaos', 'CidadaoController@index');
